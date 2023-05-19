e0036ec9fae516f0571952620c424e4fb3bba14d
Josh Claxton <jrccode@outlook.com>
2023-05-19 01:42:04 -0500

    style fixes

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -34,28 +34,36 @@ const PokemonList = () => {
     fetchPokemon()
   }, [pokemonURL])
   return (
-    <div>
-      <h3>Pokémon List</h3>
-
-      <h4>Selected Pokémon: </h4>
-      {pokemon?.name}
-      <h6>Weight:</h6>
-      {pokemon?.weight}
-      <h6>Types:</h6>
-      {pokemon?.types.map((type) => (
-        <ul key={type.slot} style={{ listStyle: 'none' }}>
-          <li>{type.slot}</li>
-        </ul>
-      ))}
-      <hr />
-      {allPokemon &&
-        allPokemon.map((pokemon) => (
-          <div key={pokemon.name}>
-            <ul>
-              <li onClick={() => setPokemonURL(pokemon.url)}>{pokemon.name}</li>
-            </ul>
-          </div>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div>
+        <h3>Pokémon List</h3>
+        {allPokemon &&
+          allPokemon.map((pokemon) => (
+            <div key={pokemon.name}>
+              <ul>
+                <li onClick={() => setPokemonURL(pokemon.url)}>
+                  {pokemon.name}
+                </li>
+              </ul>
+            </div>
+          ))}
+      </div>
+      <div style={{ position: 'fixed', right: '0' }}>
+        <h4>Selected Pokémon: </h4>
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        <br />
+        &nbsp;&nbsp;
+        <strong>{pokemon?.name.toUpperCase()}</strong>
+        <h6>Weight:</h6>
+        &nbsp;&nbsp;{pokemon?.weight}
+        <h6>Types:</h6>
+        {pokemon?.types.map((type) => (
+          <ul key={type.slot} style={{ listStyle: 'none' }}>
+            <li>{type.type.name.toUpperCase()}</li>
+          </ul>
         ))}
+        <hr />
+      </div>
     </div>
   )
 }
